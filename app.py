40862c9d89d1817c1f43e221efbbce7062643926
Mwale Kalenga <mwale.kalenga@prosites.space>
2022-05-31 20:10:26 +0200

    Added CORs.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -12,6 +13,8 @@ from resources.store import Store, StoreList
 db_url = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres", "postgresql", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mwalek'
